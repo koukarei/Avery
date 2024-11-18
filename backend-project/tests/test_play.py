8155e895c052c947898d6ce397d426fd2c643997
Zand567 <69153235+Zand567@users.noreply.github.com>
2024-11-18 13:04:41 +0900

    create test play file

---
+++ backend-project/tests/test_play.py
@@ -0,0 +1,21 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+import csv
+
+from fastapi.testclient import TestClient
+
+from main import app 
+from fastapi import UploadFile
+
+client = TestClient(app)
+
+def test_play():
+    # Create leaderboards
+
+
+    # Read csv file
+    with open('static/entries/202408_Results.csv','r') as f:
+        reader = csv.DictReader(f)
+    for row in reader:
+        pass
