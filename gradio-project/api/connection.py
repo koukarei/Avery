c0fa81c51eada8776d7e4498515be99461172d1b
koukarei <kareikou.carly@gmail.com>
2024-12-17 16:39:50 +0900

    auth fix

--- gradio-project/api/connection.py
+++ gradio-project/api/connection.py
@@ -21,19 +21,19 @@ class BearerAuth(httpx.Auth):
         self.refresh_token = refresh_token
         self.refresh_url = refresh_url
 
-    def auth_flow(self, request):
+    async def auth_flow(self, request):
         request.headers["Authorization"] = f"Bearer {self.access_token}"
         response = yield request
         if response.status_code == 401:
 
-            refresh_response = yield self.build_refresh_request()
+            refresh_response = yield await self.build_refresh_request()
             self.update_tokens(refresh_response)
 
             request.headers["Authorization"] = f"Bearer {self.access_token}"
             yield request
 
-    def build_refresh_request(self):
-        response = http_client.post(
+    async def build_refresh_request(self):
+        response = await http_client.post(
             self.refresh_url,
             data={"refresh_token": self.refresh_token}
         )
