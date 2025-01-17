a9e9b7c0518a26a07d6095729e3ba606d10887ca
koukarei <kareikou.carly@gmail.com>
2025-01-17 15:30:40 +0900

    log in bug

--- gradio-project/app.py
+++ gradio-project/app.py
@@ -220,14 +220,13 @@ def token(request: Request):
 
 @app.get("/")
 async def redirect_page(request: Request):
-    if "token" in request.session:
+    if "token" not in request.session:
         try:
             response = await read_leaderboard(request)
             assert response.status_code == 401
         except:
             return RedirectResponse(url="/avery/logout", status_code=status.HTTP_303_SEE_OTHER)
-        return RedirectResponse(url="/avery/leaderboards", status_code=status.HTTP_303_SEE_OTHER)
-    return RedirectResponse(url="/avery/logout", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/avery/leaderboards", status_code=status.HTTP_303_SEE_OTHER)
     
 @app.get("/retry")
 async def retry(request: Request):
