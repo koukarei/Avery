cdf9a9593c97739901fdbb503173feab65ac8774
koukarei <kareikou.carly@gmail.com>
2025-04-28 15:24:21 +0900

    fix

--- backend-project/backend/sql_app_2/main.py
+++ backend-project/backend/sql_app_2/main.py
@@ -2340,7 +2340,7 @@ async def check_leaderboard_playable(
                 program_id=program_id,
                 player_id=current_user.id
             )
-            if db_rounds:
+            if db_rounds is not None or db_rounds == []:
                 return schemas.LeaderboardPlayable(
                     id=leaderboard_id,
                     is_playable=False
@@ -2353,7 +2353,7 @@ async def check_leaderboard_playable(
             player_id=current_user.id
         )
 
-        if db_rounds:
+        if db_rounds is not None or db_rounds == []:
             return schemas.LeaderboardPlayable(
                 id=leaderboard_id,
                 is_playable=False
