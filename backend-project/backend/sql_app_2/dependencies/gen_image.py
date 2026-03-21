3be8dd721a281f80aca26b5135f9c8fe8147c451
kareikou <69153235+Zand567@users.noreply.github.com>
2026-03-21 13:44:46 +0900

    image gen update

--- backend-project/backend/sql_app_2/dependencies/gen_image.py
+++ backend-project/backend/sql_app_2/dependencies/gen_image.py
@@ -7,6 +7,7 @@ from PIL import Image
 from io import BytesIO
 import base64, os, requests, time
 from util import encode_image, logger_image
+from typing import Literal
 
 def save_image(url, filename):
     urllib.request.urlretrieve(url, filename)
@@ -29,8 +30,46 @@ def gen_image(sentence,size="1024x1024",quality="standard",n=1):
             continue
     return response.data[0].url
 
+def gen_image_gpt_image_1_5(prompt):
+    client = OpenAI()
+    while True:
+        try:
+            result = client.images.generate(
+                model="gpt-image-1.5",
+                prompt=prompt
+            )
 
-def get_image_gemini(prompt):
+            if result and result.data and result.data[0].b64_json:
+                break
+        except Exception as e:
+            print(f"Error: {e}")
+            continue
+    return result.data[0].b64_json
+
+def gen_image_gpt_5(prompt):
+    client = OpenAI()
+    while True:
+        try:
+            response = client.responses.create(
+                model="gpt-5",
+                input=prompt,
+                tools=[{"type": "image_generation"}],
+            )
+
+            image_data = [
+                output.result
+                for output in response.output
+                if output.type == "image_generation_call"
+            ]
+
+            if image_data:
+                break
+        except Exception as e:
+            print(f"Error: {e}")
+            continue
+    return image_data[0]
+       
+def gen_image_gemini(prompt):
     client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
 
     while True:
@@ -59,7 +98,15 @@ def get_image_gemini(prompt):
                     retry_delay = detail.get('retryDelay')
                     time.sleep(int(retry_delay.split('s')[0]))
 
-def generate_interpretion(sentence, style="in the style of Japanese Anime", model="gemini"):
+def generate_interpretion(
+    sentence, 
+    style="in the style of Japanese Anime", 
+    model="gemini"
+) -> str | None:
+    
+    # This function generates an image based on the input sentence and style using the specified model.
+    # model: "dall-e-3", "gpt-image-1.5","gpt-5", "gemini"
+
     if model == "dall-e-3":
         prompt="""
                 I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS:
@@ -71,9 +118,17 @@ def generate_interpretion(sentence, style="in the style of Japanese Anime", mode
         b_interpreted_image = BytesIO(requests.get(url).content)
         image = encode_image(image_file=b_interpreted_image)
         return image
+    elif model == "gpt-image-1.5":
+        prompt = "Hi, please create an image {style} to show that {passage}".format(passage=sentence, style=style)
+        base64_image = gen_image_gpt_image_1_5(prompt)
+        return base64_image
+    elif model == "gpt-5":
+        prompt = "Generate an image {style} to show that {passage}".format(passage=sentence, style=style)
+        base64_image = gen_image_gpt_5(prompt)
+        return base64_image
     elif model == "gemini":
         prompt="Hi, please create an image {style} to show that {passage}".format(passage=sentence, style=style)
-        pil_interpreted_image = get_image_gemini(prompt)
+        pil_interpreted_image = gen_image_gemini(prompt)
         buffer = BytesIO()
         pil_interpreted_image.save(buffer, format="PNG")
         image = base64.b64encode(buffer.getvalue()).decode("utf-8")
